9645b9e6842cb8e69b226c2844eab93a562564c0
dima <vds788502@gmail.com>
2025-11-26 01:30:11 +0400

    добавлены зависимости в файл config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,9 @@
 class Settings:
-    path_log: str = r""
-    path_sql_database: str = r""
+    path_log: str = r"Путь до файла с сохранением логов"
+    path_sql_database: str = r"Путь до бд"
+    name_table: str = "Название таблиц"
+    csv_path_file: str = r"Путь до csv файла для сохранения результата парсинга номенклатур"
+    excel_result_file: str = r"Путь до excel файла с результатом номенклатур"
 
 
 settings = Settings()
