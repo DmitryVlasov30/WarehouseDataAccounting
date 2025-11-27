c7398042979111585a3b7f6245b4f08f660d162f
dima <vds788502@gmail.com>
2025-11-28 01:24:58 +0400

    Изменены пути

--- src/config.py
+++ src/config.py
@@ -1,12 +1,18 @@
-class Settings:
-    path_log: str = r"Путь до файла с сохранением логов"
-    path_sql_database: str = r"Путь до бд"
-    name_table: str = "Название таблицы с сокращениями таблиц"
-    invoices_table: str = "Название таблиц для номенклатур"
-    csv_path_file: str = r"Путь до csv файла для сохранения результата парсинга номенклатур"
-    excel_result_file: str = r"Путь до excel файла с результатом номенклатур"
-    csv_path_comp: str = r"путь до файла куда будут сохранятся результаты сравнения"
-    excel_comp_result: str = "путь до excel файла, где будет хранится сравнения"
+from pathlib import Path
+from pydantic_settings import BaseSettings
+
+BASE_DIR = Path(__file__).parent.parent
+
+
+class Settings(BaseSettings):
+    path_log: str = rf"{BASE_DIR}\all_logs.log"
+    path_sql_database: str = rf"{BASE_DIR}\units_data.db"
+    name_table: str = "main_table"
+    invoices_table: str = "invoices_table"
+    csv_path_file: str = rf"{BASE_DIR}\output_pdf.csv"
+    excel_result_file: str = rf"{BASE_DIR}\result_file.xlsx"
+    csv_path_comp: str = rf"{BASE_DIR}\comp_file.csv"
+    excel_comp_result: str = rf"{BASE_DIR}\result_file_comp2.xlsx"
 
 
 settings = Settings()
